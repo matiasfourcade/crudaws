ab076ee9d053a018dda50c6d4bcfcdd7144a4152
matiasf <matias.f@code-care.pro>
2021-10-17 14:15:41 -0400

    setting list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import './App.css';
-import Amplify from 'aws-amplify';
+import React, {useEffect, useState} from 'react';
+import Amplify, {API, graphqlOperation } from 'aws-amplify';
 import awsConfig from './aws-exports';
-import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-Amplify.configure(awsConfig);
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listLists } from './graphql/queries';
 
+Amplify.configure(awsConfig);
+    
 function App() {
+
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+      const { data } = await API.graphql(graphqlOperation(listLists));  
+      setList(data.listLists.items);
+      console.log(data);
+    } 
+  
+  useEffect(() => {
+    fetchList();
+  }, [])
+
   return (
     <AmplifyAuthenticator>
       <div className="App">
-      <h1>Amplify app</h1>
+        <h1>Amplify app</h1>
+        
+        <h2>List</h2>
+        {
+          list.map(item => <div> {item.title}  </div>)
+        }
+        </div>
       <AmplifySignOut/>
-      
-      </div>
     </AmplifyAuthenticator>
   );
 }
